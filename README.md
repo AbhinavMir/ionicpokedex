da5579a5d76c636df302d3b8b6d4c71182429e9c
Abhinav Srivastava <36011576+AbhinavMir@users.noreply.github.com>
2020-10-27 00:14:00 +0530

    general update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ionicpokedex
 A pokedex built in Ionic for Android.
+
+```bash
+cd pokedex
+ionic serve
+```
+
+What I learnt
+- [ ] API Calls in Ionic
+- [ ] Routing
+- [ ] Infinite Scrolling
+- [ ] Search
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'home/:index',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home/:index', loadChildren: './details/details.module#DetailsPageModule' },
 ];
 
 @NgModule({
